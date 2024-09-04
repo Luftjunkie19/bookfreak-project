abac8327f9d0fd84dcbe9505efa0032c02aaa01b
Luftjunkie <kshjssjjs@gmail.com>
2024-09-04 18:30:36 +0200

    Deactivate Firebase, Prepare to Switch to Supabase

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,26 +1,26 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
-import { getFunctions } from 'firebase/functions';
-import { getStorage } from 'firebase/storage';
-import { getFirestore } from 'firebase/firestore';
+// import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
+// import { getDatabase } from 'firebase/database';
+// import { getFunctions } from 'firebase/functions';
+// import { getStorage } from 'firebase/storage';
+// import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_DOMAINNAME,
-  databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
-  projectId: process.env.NEXT_PUBLIC_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSENDERID,
-  appId: process.env.NEXT_PUBLIC_APPID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTS,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_APIKEY,
+//   authDomain: process.env.NEXT_PUBLIC_DOMAINNAME,
+//   databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
+//   projectId: process.env.NEXT_PUBLIC_PROJECTID,
+//   storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_MESSENDERID,
+//   appId: process.env.NEXT_PUBLIC_APPID,
+//   measurementId: process.env.NEXT_PUBLIC_MEASUREMENTS,
+// };
 
 
-export const app= initializeApp(firebaseConfig);
+// export const app= initializeApp(firebaseConfig);
 
-export const auth= getAuth(app);
-export const database=getDatabase(app);
-export const storage=getStorage(app);
-export const functions = getFunctions(app);
-export const firestore = getFirestore(app);
+// export const auth= getAuth(app);
+// export const database=getDatabase(app);
+// export const storage=getStorage(app);
+// export const functions = getFunctions(app);
+// export const firestore = getFirestore(app);
